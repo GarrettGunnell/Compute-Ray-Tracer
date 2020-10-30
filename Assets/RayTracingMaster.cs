f6cdfd7d6ffba32fcf955b81efe26c45c3534366
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-10-30 06:38:16 -0700

    Initialize target render texture

--- Assets/RayTracingMaster.cs
+++ Assets/RayTracingMaster.cs
@@ -1,5 +1,22 @@
 ﻿using UnityEngine;
 
 public class RayTracingMaster : MonoBehaviour {
+    
+    public ComputeShader RayTracingShader;
 
+    private RenderTexture target;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        InitRenderTexture();
+    }
+
+    private void InitRenderTexture() {
+        if (target == null || target.width != Screen.width || target.height != Screen.height) {
+            if (target != null) target.Release();
+
+            target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create();
+        }
+    }
 }
