a7f61f0b1c4863d79f9cebd0f25f35038690d64e
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-10-30 07:10:35 -0700

    Visualize UVs

--- Assets/RayTracingMaster.cs
+++ Assets/RayTracingMaster.cs
@@ -10,6 +10,10 @@ public class RayTracingMaster : MonoBehaviour {
     private void OnRenderImage(RenderTexture source, RenderTexture destination) {
         InitRenderTexture();
 
+        Camera camera = GetComponent<Camera>();
+        RayTracingShader.SetMatrix("_CameraToWorld", camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", camera.projectionMatrix.inverse);
+
         RayTracingShader.SetTexture(0, "Result", target);
         int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
