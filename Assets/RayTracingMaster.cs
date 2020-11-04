697d6e0920c7f94fdd7ebbe8986c3de64ab0eb43
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-11-03 19:02:27 -0800

    Randomize scene with a variety of spheres on start up

--- Assets/RayTracingMaster.cs
+++ Assets/RayTracingMaster.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Runtime;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class RayTracingMaster : MonoBehaviour {
     
@@ -6,16 +8,63 @@ public class RayTracingMaster : MonoBehaviour {
     public Texture SkyboxTexture;
     public Light DirectionalLight;
 
+    public Vector2 sphereRadius = new Vector2(3.0f, 8.0f);
+    public uint maxSpheres = 100;
+    public float spherePlacementRadius = 100.0f;
+
+    private ComputeBuffer _SphereBuffer;
     private RenderTexture target;
     private uint currentSample = 0;
     private Material aliasing;
 
     struct Sphere {
-        Vector3 position;
-        float radius;
-        Vector3 albedo;
-        Vector3 specular;
-    };
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    }
+
+    private void OnEnable() {
+        currentSample = 0;
+        SetUpScene();
+    }
+
+    private void SetUpScene() {
+        List<Sphere> spheres = new List<Sphere>();
+
+        for (int i = 0; i < maxSpheres; ++i) {
+            Sphere sphere = new Sphere();
+
+            sphere.radius = sphereRadius.x + Random.value * (sphereRadius.y - sphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * spherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            foreach (Sphere other in spheres) {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                    goto SkipSphere;
+            }
+
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+
+            spheres.Add(sphere);
+
+            SkipSphere:
+                continue;
+        }
+
+        _SphereBuffer = new ComputeBuffer(spheres.Count, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Sphere)));
+        _SphereBuffer.SetData(spheres);
+    }
+
+    private void OnDisable() {
+        if (_SphereBuffer != null) {
+            _SphereBuffer.Release();
+        }
+    }
 
     private void Update() {
         if (transform.hasChanged) {
@@ -39,6 +88,7 @@ public class RayTracingMaster : MonoBehaviour {
         RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
         Vector3 l = DirectionalLight.transform.forward;
         RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        RayTracingShader.SetBuffer(0, "_Spheres", _SphereBuffer);
 
         RayTracingShader.SetTexture(0, "Result", target);
         int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
